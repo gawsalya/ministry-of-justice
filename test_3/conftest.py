0812352425489c04319d18c5e4c230afd4e75d8e
gawsalya <rj_gawsalya@hotmail.co.uk>
2023-07-31 08:47:55 +0100

    testing

--- test_3/conftest.py
+++ test_3/conftest.py
@@ -1,21 +1,26 @@
+'''library for testing'''
 import pytest
 
 
 @pytest.fixture
 def fake_correct_time():
+    '''fake correct time for testing'''
     return '01:02:03'
 
 
 @pytest.fixture
 def fake_incorrect_time():
+    '''fake incorrect time for testing'''
     return '29:30:44'
 
 
 @pytest.fixture
 def bad_format():
+    '''poorly formatted time'''
     return '22:00'
 
 
 @pytest.fixture
 def not_a_string():
+    '''time as integers'''
     return (120032)
